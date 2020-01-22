b2323fcc903fc2051299780139d148c291b358e4
AsamiKamiya <petit0608@gmail.com>
2020-01-22 16:57:28 +0900

    finish until editing pokemon.

--- server/index.js
+++ server/index.js
@@ -8,11 +8,81 @@ const data = require("./data");
 const schema = buildSchema(`
   type Pokemon {
     id: String
-    name: String!
+    name: String
+    classification: String
+    types: [String]
+    resistant: [String]
+    weakness: [String]
+    weight: Weight
+    height: Height
+    fleeRate: Float
+    evolutionRequirements: EvolutionRequirement
+    evolutions: [Evolutions]
+    maxCP: Int
+    maxHP: Int
+    attacks: AttackType
+  }
+
+  type Weight {
+    minimum: String
+    maximum: String
+  }
+  type Height {
+    minimum: String
+    maximum: String
+  }
+  type EvolutionRequirement {
+    amount: Int
+    name: String
+  }
+  type AttackType {
+    fast : [Attack]
+    special: [Attack]
+  }
+  type Attack {
+    name: String
+    type: String
+    damage: Int
+  }
+  type Evolutions {
+    id: String
+    name : String
   }
   type Query {
     Pokemons: [Pokemon]
-    Pokemon(name: String!): Pokemon
+    Pokemon(name: String,id: String): Pokemon
+    Types: [String]
+    Attacks: AttackType
+    Attack(type: String): [Attack]
+    PokemonByType(name: String): [Pokemon]
+    PokemonByAttack(name: String): [Pokemon]
+  }
+  input pokemonInput {
+    id: String
+    name: String
+    classification: String
+  }
+  input attackInput {
+    fastOrSpecial: String
+    name: String
+    type: String
+    damage: Int
+  }
+  input typeInput {
+    name: String
+  }
+
+  input pokemonEdit {
+    name: String
+    editField: String
+    editValue: String
+  }
+
+  type Mutation {
+    addPokemon(input: pokemonInput): Pokemon
+    addAttack(input: attackInput): Attack
+    addType(input: typeInput): [String]
+    editPokemon(input: pokemonEdit): Pokemon
   }
 `);
 
@@ -22,7 +92,76 @@ const root = {
     return data.pokemon;
   },
   Pokemon: (request) => {
-    return data.pokemon.find((pokemon) => pokemon.name === request.name);
+    return data.pokemon.find((pokemon) => {
+      if (pokemon.id === request.id || pokemon.name === request.name) {
+        return pokemon;
+      }
+    });
+  },
+  Types: () => {
+    return data.types;
+  },
+  Attacks: () => {
+    return data.attacks;
+  },
+  Attack: (request) => {
+    // console.log(data.pokemon[0].types);
+    return data.attacks[request.type];
+  },
+  PokemonByType: (request) => {
+    const results = [];
+    data.pokemon.find((pokemon) => {
+      if (pokemon.types.includes(request.name)) {
+        results.push(pokemon);
+      }
+    });
+    return results;
+  },
+  PokemonByAttack: (request) => {
+    const results = [];
+    data.pokemon.find((pokemon) => {
+      const allAttacks = pokemon.attacks.fast.concat(pokemon.attacks.special);
+      for (const elm of allAttacks) {
+        if (elm.name === request.name) {
+          results.push(pokemon);
+        }
+      }
+    });
+    return results;
+  },
+  addPokemon: (request) => {
+    const newPokemon = {
+      name: request.input.name,
+      id: request.input.id,
+      classification: request.input.classification,
+    };
+    data.pokemon.push(newPokemon);
+    return newPokemon;
+  },
+  addAttack: (request) => {
+    const newAttack = {
+      name: request.input.name,
+      type: request.input.type,
+      damage: request.input.damage,
+    };
+    console.log(newAttack);
+    data.attacks[request.input.fastOrSpecial].push(newAttack);
+    console.log(data.attacks[request.input.fastOrSpecial]);
+    return newAttack;
+  },
+  addType: (request) => {
+    const newType = request.input.name;
+    data.types.push(newType);
+    return data.types;
+  },
+  editPokemon: (request) => {
+    for (const poke of data.pokemon) {
+      if (poke.name === request.input.name) {
+        console.log(poke[request.input.editField]);
+        poke[request.input.editField] = request.input.editValue;
+        return poke;
+      }
+    }
   },
 };
 
